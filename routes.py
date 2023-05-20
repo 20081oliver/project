3f5c166764a27218a77c446784d8f9d528c1f9c0
oliver <20081@burnside.school.nz>
2023-05-20 19:02:37 +1200

    modified routes and all songs

--- routes.py
+++ routes.py
@@ -18,11 +18,11 @@ def about():
 def all_songs():
     conn = sqlite3.connect('song.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM song')
+    cur.execute('SELECT name FROM song')
     results = cur.fetchall()
     print(results)
     return render_template("all_songs.html", results=results)
 
 
 if __name__ == "__main__":
-    app.run(debug = True)    
+    app.run(debug = True)
