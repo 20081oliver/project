b8919af774711bfce6873c856d72850cb70e3eab
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-07-19 14:18:47 +1200

    pep8 modifications

--- routes.py
+++ routes.py
@@ -14,6 +14,7 @@ def home():
 def about():
     return render_template("about.html")
 
+
 @app.route('/all_songs')
 def all_songs():
     conn = sqlite3.connect('song.db')
@@ -22,6 +23,7 @@ def all_songs():
     results = cur.fetchall()
     print(results)
     return render_template("all_songs.html", results=results)
+
 
 @app.post('/add_a_song')
 def add_a_song():
@@ -32,11 +34,12 @@ def add_a_song():
     conn.commit()
     results = cur.fetchall()
     return redirect("all_songs")
-  
+
 
 @app.route('/add_song', methods=['POST', 'GET'])
 def add_song():
     return render_template("add_song.html")
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
