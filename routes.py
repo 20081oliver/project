43e295ee398fb8d9c84d6f9befcd5790590bdff3
oliver <20081@burnside.school.nz>
2023-07-24 21:20:03 +1200

    flask 8 fixing and beginning to add a delete feature

--- routes.py
+++ routes.py
@@ -14,6 +14,7 @@ def home():
 def about():
     return render_template("about.html")
 
+
 @app.route('/all_songs')
 def all_songs():
     conn = sqlite3.connect('song.db')
@@ -23,6 +24,7 @@ def all_songs():
     print(results)
     return render_template("all_songs.html", results=results)
 
+
 @app.post('/add_a_song')
 def add_a_song():
     conn = sqlite3.connect('song.db')
@@ -31,12 +33,16 @@ def add_a_song():
     cur.execute(sql, (request.form['name'], request.form['artist'], request.form['album']))
     conn.commit()
     results = cur.fetchall()
+    print(results)
     return redirect("all_songs")
-  
+
+
+# @app.route('/delete/<int:id>')
 
 @app.route('/add_song', methods=['POST', 'GET'])
 def add_song():
     return render_template("add_song.html")
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
