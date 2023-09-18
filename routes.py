f24f9fc71dfefa77e895bafe335b56503c575953
oliver <20081@burnside.school.nz>
2023-09-18 21:45:43 +1200

    added comments, message flashing and css

--- routes.py
+++ routes.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 import sqlite3
 
 
 app = Flask(__name__)
+app.secret_key = "flash"  # used for flasking
 conn = sqlite3.connect('song.db')
 conn = sqlite3.connect('song.db', check_same_thread=False)
 cur = conn.cursor()
+
+# home tab
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
+# about tab
+
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+# route that displays all the songs added
 
 
 @app.route('/all_songs')
@@ -25,6 +32,8 @@ def all_songs():
     print(results)
     return render_template("all_songs.html", results=results)
 
+# adding a song code
+
 
 @app.post('/add_a_song')
 def add_a_song():
@@ -33,20 +42,25 @@ def add_a_song():
     conn.commit()
     results = cur.fetchall()
     print(results)
+    flash('Song submitted!')
     return redirect("all_songs")
+
+
+# delelte code
 
 
 @app.route('/delete/<name>')
 def delete_song(name):
     conn = sqlite3.connect("song.db")
-
     cur.execute("DELETE FROM song WHERE name = ?", (name,))
     conn.commit()
     print("name")
     return redirect("/admin_all_songs")
 
+# admin login page
 
-password = "1234"
+
+password = "1234"  # password to access admin
 typed = False
 
 
@@ -58,10 +72,14 @@ def adminLogin():
         formRequest = request.form['password']
         if formRequest == password:
             typed = True
-            return redirect("/admin_all_songs")
+            flash("Login successful!")
+            return redirect("/admin_all_songs")  # if the password is correct, the user will be redirected to "admin_all_songs" and get flashed with "Login successfull!"
         else:
+            flash('Incorrect password')  # if the password is incorrect a message will flash "Incorrect Password"
             return redirect("/admin/login")
     return render_template("login.html")
+
+# how admins view every song
 
 
 @app.route('/admin_all_songs')
